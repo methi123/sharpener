e998a347a8e639fccc27ffb3f684e6e21f3d391a
methi123 <Metheswar@gmail.com>
2023-03-29 17:19:05 +0530

    second commit

--- sharpener.js
+++ sharpener.js
@@ -1,1 +1,6 @@
-console.log(document.title);
+var header = document.getElementById('header-title');
+header.innerText='metheswar';
+header.style.border = 'solid 3px #000';
+var items = document.getElementById('title');
+items.style.color = 'green';
+items.style.fontWeight = 'bold';
