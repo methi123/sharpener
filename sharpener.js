07ac063d6ac89592edc7310d80a8a5b4710b74ba
methi123 <Metheswar@gmail.com>
2023-03-29 23:11:51 +0530

    task 5

--- sharpener.js
+++ sharpener.js
@@ -7,3 +7,6 @@ for(var i=0;i<items.length;i++){
     items[i].style.fontWeight = 'bold';
 }
 console.log("done");
+
+var item1 = document.getElementsByTagName('li');
+document.getElementById("demo").innerHTML = item1[1].innerHTML;
