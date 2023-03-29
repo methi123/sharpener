e05f3e4f238ee55ec8bdf5d4af2b1f52b72dd84e
methi123 <Metheswar@gmail.com>
2023-03-29 23:22:04 +0530

    task 6

--- sharpener.js
+++ sharpener.js
@@ -1,12 +1,6 @@
 var header = document.getElementById('header-title');
 header.innerText='metheswar';
 header.style.border = 'solid 3px #000';
-var items = document.getElementsByClassName('list-group-item');
-items[2].style.color = 'green';
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight = 'bold';
-}
-console.log("done");
-
-var item1 = document.getElementsByTagName('li');
-document.getElementById("demo").innerHTML = item1[1].innerHTML;
+var items = document.querySelectorAll("li");
+items[1].style.color = 'green';
+items[2].style.visibility = "hidden";
