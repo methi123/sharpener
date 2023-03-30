effd8c6d13a2e053e8694583fb3f81065ef0cc46
methi123 <Metheswar@gmail.com>
2023-03-30 22:54:10 +0530

    task 7

--- sharpener.js
+++ sharpener.js
@@ -1,6 +1,9 @@
-var header = document.getElementById('header-title');
-header.innerText='metheswar';
-header.style.border = 'solid 3px #000';
-var items = document.querySelectorAll("li");
-items[1].style.color = 'green';
-items[2].style.visibility = "hidden";
+const headerTitle = document.querySelector("#header-title");
+const helloText = document.createTextNode("Hello ");
+headerTitle.parentElement.insertBefore(helloText, headerTitle);
+
+
+const item1 = document.querySelector("#items li:first-child");
+const helloListItem = document.createElement("li");
+helloListItem.appendChild(document.createTextNode("Hello "));
+item1.parentElement.insertBefore(helloListItem, item1);
