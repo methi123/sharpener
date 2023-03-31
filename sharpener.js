009664c2340d58dc21c0aa3b37e7a178ed12ead0
methi123 <Metheswar@gmail.com>
2023-03-31 22:21:10 +0530

    task 9

--- sharpener.js
+++ sharpener.js
@@ -6,6 +6,8 @@ var filter = document.getElementById('filter');
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
 // Add item
 function addItem(e){
@@ -13,6 +15,7 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var description = document.getElementById('description').value;
 
   // Create new li element
   var li = document.createElement('li');
@@ -20,18 +23,23 @@ function addItem(e){
   li.className = 'list-group-item';
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(" "+description));
 
   // Create del button element
   var deleteBtn = document.createElement('button');
+  var edit = document.createElement('button');
 
   // Add classes to del button
   deleteBtn.className = 'btn btn-danger btn-sm float-end delete';
+  edit.className = 'btn btn-info btn-sm float-end edit';
 
   // Append text node
   deleteBtn.appendChild(document.createTextNode('X'));
+  edit.appendChild(document.createTextNode('edit'));
 
   // Append button to li
   li.appendChild(deleteBtn);
+  li.appendChild(edit);
 
   // Append li to list
   itemList.appendChild(li);
@@ -47,9 +55,20 @@ function removeItem(e){
   }
 }
 
-let editButtons = document.querySelectorAll('.edit');
-for (let i = 0; i < editButtons.length; i++) {
-  editButtons[i].addEventListener('click', function() {
-    // Add the edit functionality here
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    var description = item.childNodes[1].textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
   });
 }
