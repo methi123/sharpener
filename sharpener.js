cf3ed0064ce3212a22d11662f10542f7302ffa17
methi123 <Metheswar@gmail.com>
2023-04-01 00:12:43 +0530

    task 13

--- sharpener.js
+++ sharpener.js
@@ -1,5 +1,6 @@
 const loginForm = document.getElementById("login-form");
 
+
 loginForm.addEventListener("submit", function(event) {
   event.preventDefault(); // prevent default form submission behavior
 
@@ -11,15 +12,29 @@ loginForm.addEventListener("submit", function(event) {
   }
   let display = document.createElement('p');
  display.className = 'display';
- display.textContent = username+' '+password;
+ display.textContent = username+' '+password+' ';
+ let delete_btn = document.createElement('button');
+ delete_btn.className = 'delete';
+ delete_btn.textContent = 'X';
 
  let task = document.getElementById('task');
  task.appendChild(display);
+ display.appendChild(delete_btn);
+
+ 
+
 
   // store user details in local storage
 
   localStorage.setItem("object",JSON.stringify(obj))
 
 });
- 
-
+let deleteitem = document.getElementById('task');
+deleteitem.addEventListener('click', remove);
+function remove(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure?')){
+            var li = e.target.parentElement;
+            task.removeChild(li);
+        }
+}};
