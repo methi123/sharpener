575f01d0ae910fa4dc85827e948d4dd7456b3828
methi123 <Metheswar@gmail.com>
2023-03-31 21:43:51 +0530

    task 8

--- sharpener.js
+++ sharpener.js
@@ -1,9 +1,55 @@
-const headerTitle = document.querySelector("#header-title");
-const helloText = document.createTextNode("Hello ");
-headerTitle.parentElement.insertBefore(helloText, headerTitle);
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
 
-const item1 = document.querySelector("#items li:first-child");
-const helloListItem = document.createElement("li");
-helloListItem.appendChild(document.createTextNode("Hello "));
-item1.parentElement.insertBefore(helloListItem, item1);
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-end delete';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+let editButtons = document.querySelectorAll('.edit');
+for (let i = 0; i < editButtons.length; i++) {
+  editButtons[i].addEventListener('click', function() {
+    // Add the edit functionality here
+  });
+}
