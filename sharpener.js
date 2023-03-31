bad2d21100a7cdc02712ca01f90b554907f963d1
methi123 <Metheswar@gmail.com>
2023-04-01 01:00:54 +0530

    task 14

--- sharpener.js
+++ sharpener.js
@@ -15,11 +15,15 @@ loginForm.addEventListener("submit", function(event) {
  display.textContent = username+' '+password+' ';
  let delete_btn = document.createElement('button');
  delete_btn.className = 'delete';
- delete_btn.textContent = 'X';
+ delete_btn.textContent = 'X'; 
+ let edit_btn = document.createElement('button');
+ edit_btn.className = 'edit';
+ edit_btn.textContent = 'Edit';
 
  let task = document.getElementById('task');
  task.appendChild(display);
  display.appendChild(delete_btn);
+ display.appendChild(edit_btn);
 
  
 
@@ -36,5 +40,23 @@ function remove(e){
         if(confirm('Are You Sure?')){
             var li = e.target.parentElement;
             task.removeChild(li);
+
         }
-}};
+    
+}
+    else if(e.target.classList.contains('edit')){
+        if(confirm('Are You Sure?')){
+
+            var li = e.target.parentElement;
+            let user = li.textContent.split(' ')[0];
+            let pass = li.textContent.split(' ')[1];
+           let user_input = document.getElementById('username');
+           user_input.value=user;
+           let pass_input = document.getElementById('password');
+           pass_input.value=pass;
+            task.removeChild(li);
+            
+
+        }
+    }
+};
