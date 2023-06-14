771987b81399a5565067a434d61e5d8881449ce6
methi123 <94212555+methi123@users.noreply.github.com>
2023-06-14 13:06:23 +0530

    Update sharpener.js

--- sharpener.js
+++ sharpener.js
@@ -1,5 +1,13 @@
 const loginForm = document.getElementById("login-form");
+const task = document.getElementById('task');
 
+// Retrieve stored orders from local storage
+const storedOrders = JSON.parse(localStorage.getItem("orders")) || [];
+
+// Display stored orders on page load
+storedOrders.forEach(order => {
+  displayOrder(order);
+});
 
 loginForm.addEventListener("submit", function(event) {
   event.preventDefault(); // prevent default form submission behavior
@@ -11,59 +19,64 @@ loginForm.addEventListener("submit", function(event) {
     amount,
     description,
     category
-  }
+  };
 
-  let display = document.createElement('p');
- display.className = 'display';
- display.textContent = amount+' '+description+' '+category+' ';
- let delete_btn = document.createElement('button');
- delete_btn.className = 'delete';
- delete_btn.textContent = 'X'; 
- let edit_btn = document.createElement('button');
- edit_btn.className = 'edit';
- edit_btn.textContent = 'Edit';
+  // Store the new order in local storage
+  storedOrders.push(obj);
+  localStorage.setItem("orders", JSON.stringify(storedOrders));
 
- let task = document.getElementById('task');
- task.appendChild(display);
- display.appendChild(delete_btn);
- display.appendChild(edit_btn);
+  // Display the new order on the page
+  displayOrder(obj);
 
- 
-
-
-  // store user details in local storage
-
-  localStorage.setItem("object",JSON.stringify(obj))
-
+  // Clear input fields
+  document.getElementById("amount").value = '';
+  document.getElementById("description").value = '';
+  document.getElementById("category").value = '';
 });
-let deleteitem = document.getElementById('task');
-deleteitem.addEventListener('click', remove);
-function remove(e){
-    if(e.target.classList.contains('delete')){
-        if(confirm('Are You Sure?')){
-            var li = e.target.parentElement;
-            task.removeChild(li);
 
-        }
-    
+function displayOrder(order) {
+  let display = document.createElement('p');
+  display.className = 'display';
+  display.textContent = order.amount + ' ' + order.description + ' ' + order.category + ' ';
+  let delete_btn = document.createElement('button');
+  delete_btn.className = 'delete';
+  delete_btn.textContent = 'X';
+  let edit_btn = document.createElement('button');
+  edit_btn.className = 'edit';
+  edit_btn.textContent = 'Edit';
+
+  display.appendChild(delete_btn);
+  display.appendChild(edit_btn);
+  task.appendChild(display);
 }
-    else if(e.target.classList.contains('edit')){
-        if(confirm('Are You Sure?')){
 
-            var li = e.target.parentElement;
-            let user = li.textContent.split(' ')[0];
-            let pass = li.textContent.split(' ')[1];
-            let categ = li.textContent.split(' ')[2];
-           let user_input = document.getElementById('amount');
-           user_input.value=user;
-           let pass_input = document.getElementById('description');
-           pass_input.value=pass;
-           let category_input = document.getElementById('category');
-           category_input.value = categ.value;
+task.addEventListener('click', function(e) {
+  if (e.target.classList.contains('delete')) {
+    if (confirm('Are You Sure?')) {
+      var li = e.target.parentElement;
+      task.removeChild(li);
 
-            task.removeChild(li);
-            
-
-        }
+      // Remove the deleted order from stored orders in local storage
+      const orderText = li.textContent.trim();
+      const deletedOrder = storedOrders.find(order => {
+        return order.amount + ' ' + order.description + ' ' + order.category + ' ' === orderText;
+      });
+      const deletedOrderIndex = storedOrders.indexOf(deletedOrder);
+      storedOrders.splice(deletedOrderIndex, 1);
+      localStorage.setItem("orders", JSON.stringify(storedOrders));
     }
-};
+  } else if (e.target.classList.contains('edit')) {
+    if (confirm('Are You Sure?')) {
+      var li = e.target.parentElement;
+      let amount = li.textContent.split(' ')[0];
+      let description = li.textContent.split(' ')[1];
+      let category = li.textContent.split(' ')[2];
+
+      document.getElementById('amount').value = amount;
+      document.getElementById('description').value = description;
+      document.getElementById('category').value = category;
+
+      task.removeChild(li);
+    }
+  }
+});
